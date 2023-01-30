0b32b53de3b4fdcb2f047c793394f789990c15e9
TaeBbong <mok05289@korea.ac.kr>
2023-01-30 23:53:58 +0900

    add: models

--- spot/models.py
+++ spot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Spot(models.Model()):
+    def __init__(self):
+        return
+    def __str__(self):
+        return self.name
+    
--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model()):
+    def __init__(self):
+        return
+    def __str__(self):
+        return self.name
